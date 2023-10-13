4f196e4110128f2714dd4b544741ba68a0a65287
Vladimir <evazyled@gmail.com>
2023-10-13 18:20:21 +0300

    start PeoplePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import style from "./App.module.scss";
+import cn from "classnames";
+import { getApi } from "./utils/network";
 
 function App() {
+  useEffect(() => {
+    getApi();
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className={cn(style.red, style.text)}>hello</h1>
     </div>
   );
 }
